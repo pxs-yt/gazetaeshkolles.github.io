da317193fbb564f34b26734b2edf11e0fd14f347
pxs-yt <158753404+pxs-yt@users.noreply.github.com>
2024-03-01 22:04:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+if (location.pathname.match(/fullcpgrid/i) ? true : false) {
+  document.documentElement.style.fontSize = "64px"
+}
+
+const button = document.getElementById("button");
+button.addEventListener("click", function() {
+  window.location.href = "https://shkollaprendushi.com";
+});
